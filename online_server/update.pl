550703e40ae426283312a19ef60295cf0b861ff5
Alex Amiryan <alex@amiryan.org>
2009-11-10 02:26:38 +0400

    Added GPL and wrote README

--- online_server/update.pl
+++ online_server/update.pl
@@ -1,4 +1,24 @@
 #!/usr/bin/perl
+#
+# update.pl - update online server with latest version
+# Copyright (C) 2008 Alex Amiryan
+#
+# This file is part of Web-CVS-Tools
+#
+# Web-CVS-Tools is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Web-CVS-Tools is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+
 use CGI qw/:standard/;
 use CGI::Cookie;
 
